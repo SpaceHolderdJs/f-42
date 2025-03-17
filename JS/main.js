716473ce960588abfedc2a189b4def8762e5cb8f
Igor <sergienko339@gmail.com>
2025-03-17 22:01:07 +0200

    js l1

---
+++ JS/main.js
@@ -0,0 +1,56 @@
+console.log("Hello world");
+
+console.log(10 + 5);
+console.log(10 - 5);
+console.log(10 * 5);
+console.log(10 / 5);
+
+const sum = 1000 + 500;
+
+const sumOfThreeElements = (10 + 10 + 10) * 10;
+const myName = "Igor";
+
+console.log(sumOfThreeElements);
+console.log(myName);
+
+const a = 100;
+const b = 20;
+const c = 80;
+
+const sumOfAbc = a + b + c;
+console.log(sumOfAbc);
+
+const diffOfAb = b - a;
+console.log(diffOfAb);
+
+const message = "Hello everyone! 😊";
+console.log(message);
+
+const firstLetter = message[0];
+const secondLetter = message[1];
+console.log(firstLetter);
+console.log(secondLetter);
+
+console.log(message[5], message[6], message[10], message[100], message[-1]);
+
+const firstName = "Igor";
+const secondName = "Sergienko";
+const space = " ";
+
+console.log(firstName + space + secondName);
+
+// Завдання
+//
+const firstWord = "Java";
+const secondWord = "Script";
+const thirdWord = "Forever";
+// Вивести у консоль браузера речення з 3 вказаних констант
+
+const a1 = 10;
+const a2 = 51;
+const a3 = 60;
+const a4 = 30;
+
+const avg = 0;
+// Порахувати середнє арифметичне чисел (a1, a2, a3, a4)
+// Вивести у консоль

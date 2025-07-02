c4018ab39df1fae0c510c7f99f35f5d7267b13fd
Igor <sergienko339@gmail.com>
2025-07-02 21:58:32 +0300

    JS DOM

--- JS/Set/main.js
+++ JS/Set/main.js
@@ -35,6 +35,9 @@ set1.add('user-1');
 set1.add('user-1');
 set1.add('user-2');
 
+console.log(set1.difference(new Set(['user-3', 'user-1'])), 'difference');
+console.log(set1.intersection(new Set(['user-1', 'user-3'])), 'intersection');
+
 console.log(set1, 'after duplicating the values');
 
 const map1 = new Map();
@@ -59,18 +62,49 @@ console.log(uniqueUsers, 'uniqueUsers');
 
 // Завдання
 const unique = (arr) => {
+  return [...new Set(arr)];
   // Set
   // Написати логіку, що поверне унікальні значення у вигляді масиву з функції
 };
+
+console.log(unique([1, 1, 1, 2, 3, 4, 4, 2]), 'unique default test');
+console.log(unique(['hello', 'hi', 'hi', 'hello'], 'unique test 2'));
+console.log(unique([true, false, true, false]), 'unique test 3');
 
 // unique([1,1,2]) => [1,2]
 // unique([true, true, true]) => [true]
 // unique(["Igor", "Alex", "Alex"]) => ["Igor", "Alex"]
 
 const uniqueObjects = (arr) => {
+  return arr.filter((obj, index) => {
+    const indexOfElement = arr.findIndex((objToFindIndex) => {
+      let isEqual = true;
+
+      for (const key in obj) {
+        if (obj[key] !== objToFindIndex[key]) {
+          isEqual = false;
+          break;
+        }
+      }
+
+      return isEqual;
+    });
+
+    return indexOfElement === index;
+  });
   // filter, findIndex, arr;
   // Написати логіку, що поверне унікальні значення у вигляді масиву з функції
   // лише objects (масив об`єктів)
 };
 
 // uniqueObjects([{a: 10}, {a: 10}, {a: 20}]) => [{a: 10}, {a: 20}]
+
+console.log(uniqueObjects([{ a: 10 }, { a: 10 }, { a: 20 }]), 'uniqueObjects');
+console.log(
+  uniqueObjects([
+    { a: 10, b: 10, role: 'User' },
+    { a: 10, b: 10, role: 'Customer' },
+    { a: 20, b: 100 },
+  ]),
+  'uniqueObjects'
+);
